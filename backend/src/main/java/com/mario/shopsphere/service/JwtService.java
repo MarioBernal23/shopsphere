c27cd20567c6154c0dc5f91f90e94fea2ad8b1ed
Mario <mariobernal0980@gmail.com>
2026-08-06 16:12:11 +0200

    Decode JWT and restore user session

--- backend/src/main/java/com/mario/shopsphere/service/JwtService.java
+++ backend/src/main/java/com/mario/shopsphere/service/JwtService.java
@@ -26,6 +26,7 @@ public class JwtService {
 
         return Jwts.builder()
                 .subject(user.getEmail())
+                .claim("role", user.getRole().name())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(key)
--- frontend/src/context/AuthenticationContext.jsx
+++ frontend/src/context/AuthenticationContext.jsx
@@ -1,33 +1,49 @@
 import { createContext } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+
 const AuthenticationContext = createContext();
 
 function AuthenticationProvider({children}) {
 
 
     const [authenticated, setAuthenticated] = useState(false);
+    const [email, setEmail] = useState(null);
+    const [role, setRole] = useState(null);
 
     useEffect(() => {
             const token = localStorage.getItem("token");
 
         if (token) {
-            setAuthenticated(true);
+
+            authenticate(token);
         }
     }, []);
 
+    function authenticate(token) {
+        const user = jwtDecode(token);
+
+        setEmail(user.sub)
+        setRole(user.role)
+
+        setAuthenticated(true);
+    }
     function login(token) {
-    localStorage.setItem("token", token);
-    setAuthenticated(true);
+        localStorage.setItem("token", token);
+
+        authenticate(token)
     }
 
     function logout() {
         localStorage.removeItem("token");
         setAuthenticated(false);
+        setEmail(null)
+        setRole(null)
     }
 
     return (
-        <AuthenticationContext.Provider value={{ authenticated, login, logout }}>
+        <AuthenticationContext.Provider value={{ authenticated, login, logout, email, role }}>
             {children}
         </AuthenticationContext.Provider>
     );

ad2ac2029f25c7ecc3183183551dda4387341596
Mario <mariobernal0980@gmail.com>
2026-08-06 15:50:41 +0200

    Implement reactive navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { AuthenticationContext } from "../context/AuthenticationContext"
 
 function Navbar() {
+
+    const { authenticated } = useContext(AuthenticationContext);
+    
     return (
         <nav>
             <div><Link to="/"><h2>ShopSphere</h2></Link></div>
             <div>
                 <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
+                {
+                    authenticated ? (
+                        <Link to ="/">Logout</Link>
+                    ) : (
+                        <Link to="/login">Login</Link>
+                    )
+                }
+
             </div>
         </nav>
     )

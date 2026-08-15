6b1d3c7410c776fe36204fd5d3dcaeeb7aed602d
Mario <mariobernal0980@gmail.com>
2026-08-15 22:09:32 +0200

    update homepage main button

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react"
+import { AuthenticationContext } from "../context/AuthenticationContext"
 import "../styles/home.css";
 
 function HomePage() {
+
+  const { authenticated } = useContext(AuthenticationContext);
+
   return (
     <div className="home">
       <h1>ShopSphere</h1>
       <p>Everything you need, all in one place.</p>
       <p>Discover our products</p>
-      <Link className="home-button" to="/products">View products</Link>
+      
+      {authenticated ? (
+        <Link className="home-button" to="/products">
+            View products
+        </Link>
+      ) : (
+        <Link className="home-button" to="/login">
+            Sign in
+        </Link>
+      )}
     </div>
   );
 }
